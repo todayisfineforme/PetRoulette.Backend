3f4b640ba88c8b704983a21fd69957eeb63256ec
Nathan Hall <Nashing1@gmail.com>
2020-07-08 18:21:37 -0500

    corrected syntax error

--- schema.sql
+++ schema.sql
@@ -1,24 +1,24 @@
-CREATE DATABASE "pet_adopter";
+CREATE DATABASE pet_adopter;
 
-USE "pet_adopter";
+USE pet_adopter;
 
-CREATE TABLE `pet_adopter`.`users` (
-    'UserID' INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    'Email' VARCHAR(256) NOT NULL,
-    'Location' INT UNSIGNED NULL,
-    'PetPreference' SET('dog', 'cat', 'both', 'other', 'any') NULL,
-    PRIMARY KEY ('UserID'));
+CREATE TABLE users (
+    `UserID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Email` VARCHAR(256) NOT NULL,
+    `Location` INT UNSIGNED NULL,
+    `PetPreference` SET('dog', 'cat', 'both', 'other', 'any') NULL,
+    PRIMARY KEY (`UserID`));
 
 
-CREATE TABLE `pet_adopter`.`wishlist` (
-    'IDWishlist' INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    'UserID' INT UNSIGNED NOT NULL,
-    'PetID' INT UNSIGNED NOT NULL,
-    'ImageURL' VARCHAR(256) NULL,
-    PRIMARY KEY ('IDWishlist'),
-    INDEX 'UserID_idx' ('UserID' ASC),
-    CONSTRAINT 'UserID'
-    FOREIGN KEY ('UserID')
-    REFERENCES 'pet_adopter'.'users' ('UserID')
+CREATE TABLE wishlist (
+    `IDWishlist` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `UserID` INT UNSIGNED NOT NULL,
+    `PetID` INT UNSIGNED NOT NULL,
+    `ImageURL` VARCHAR(256) NULL,
+    PRIMARY KEY (`IDWishlist`),
+    INDEX `UserID_idx` (`UserID` ASC),
+    CONSTRAINT `UserID`
+    FOREIGN KEY (`UserID`)
+    REFERENCES `pet_adopter`.`users` (`UserID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
