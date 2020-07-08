e38d77698bd8c1504dbb6ed3dca39b766c89679b
Alex <achriscurtis@gmail.com>
2020-07-07 19:25:45 -0500

    travis

---
+++ tests/travis.test.js
@@ -0,0 +1,7 @@
+describe("travis", () => {
+    describe("travis", () => {
+        it("should pass travis", () => {
+            expect({status: true}).toEqual({status: true});
+        });
+    });
+});
